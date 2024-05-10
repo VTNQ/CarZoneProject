02653a619e1a38e206590a113afef95fdbef0c55
Kyos22 <nguyenthanhcongvt123@gmail.com>
2024-05-10 17:30:12 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Browser
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit1 <code>src/App.js</code> and save to reload.
-        </p>
-        <h1></h1>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
-    </div>
-=======
- <></>
->>>>>>> release
+ 
   );
 }
 
