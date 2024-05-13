bbe5442faa866f5ad28c867c991d3a048b2521c8
Kyos22 <nguyenthanhcongvt123@gmail.com>
2024-05-13 14:04:31 +0700

    cong update

--- client/src/Components/ClientUI/Homepage/Homepage.jsx
+++ client/src/Components/ClientUI/Homepage/Homepage.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
-import { useEffect } from 'react'
+import { useEffect,useRef } from 'react'
 import Menu from '../../Menu/Menu'
 import homepage from '../../assets/images/homepage/homepage-bg.jpg'
 import homepage2 from '../../assets/images/homepage/homepage2-bg.jpg'
 import '../Homepage/Homepage.css'
+import car1 from '../../assets/images/cars/mercedes-benz.jpg'
+import Counter from '../../SubFeature/Counter'
+import Select from 'react-select';
+
+
 export const Homepage = () => {
     const cards = [
         { id: 1, title: "Card 1", description: "Description for Card 1" },
-        { id: 2, title: "Card 2", description: "Description for Card 2" },
+        { id: 2, title: "Card 2", description: "Description for Card 2 Descriptiovdfvfdn for Card 2 Dedsjskscscription for Card 2 Description for Card 2" },
         { id: 3, title: "Card 3dcsdjcnskdc", description: "Description for Card 3" },
         { id: 4, title: "Card 4", description: "Description for Card 4" },
         { id: 5, title: "Card 5", description: "Description for Card 5" },
         { id: 6, title: "Card 6", description: "Description for Card 5" },
-        { id: 7, title: "Card 7", description: "Description for Card 5" },
-       
+        
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -23,9 +27,7 @@ export const Homepage = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) =>
-                prevIndex >= totalSlides - 1 ? 0 : prevIndex + 1
-            );
+            setCurrentIndex(prevIndex => (prevIndex + 1) % totalSlides);
         }, 4000); // Change card every 4 seconds
 
         return () => clearInterval(interval);
@@ -34,6 +36,25 @@ export const Homepage = () => {
     const handleDotClick = (index) => {
         setCurrentIndex(index);
     };
+
+    //count start end
+    const options = [
+        { value: 'all', label: 'All Make' },
+        { value: 'audi', label: 'Audi' },
+        { value: 'bmw', label: 'BMW' },
+        { value: 'chevrolet', label: 'Chevrolet' },
+        // Thêm các tùy chọn khác
+      ];
+    
+      const customStyles = {
+        option: (provided, state) => ({
+          ...provided,
+          height: '100px',  // Tăng chiều cao của từng tùy chọn
+          padding: '10px', // Thêm padding để văn bản không dính vào cạnh
+          alignItems: 'center', // Căn giữa nội dung tùy chọn theo chiều dọc
+          display: 'flex'
+        }),
+      };
   return (
     
     <>
@@ -51,7 +72,7 @@ export const Homepage = () => {
                 <h1 className='text-banner'>base mspr from <b>$450,870*</b></h1>
             </div>
         </div>
-        <div className='section2 w-full mt-[86px] bg-[#FF5500] h-[30vh] bg-yellow-400 grid grid-cols-3 px-9 gap-3'>
+        <div className='section2 w-full mt-[86px] bg-[#FF5500] h-[30vh] grid grid-cols-3 px-9 gap-3'>
             <div className='card-infomation flex  py-[50px] pl-4'>
                 <div className='circle-icon'>
                 <i class='bx bxs-user-circle text-[3rem]' style={{color:'#f6f0f0'}}  ></i>
@@ -82,9 +103,9 @@ export const Homepage = () => {
             
             
         </div>
-        <div className='section3 bg-pink-400 w-full h-[130vh] relative'>
-            <img className='h-[130vh] object-cover' src={homepage2} alt="" />
-            <div className='absolute h-[111.5vh] overlay inset-0 bg-black  opacity-40 z-[1]'></div>
+        <div className='section3 bg-pink-400 w-full h-[145vh] relative'>
+            <img className='h-[145vh] object-cover' src={homepage2} alt="" />
+            <div className='absolute h-[125vh] overlay inset-0  bg-black opacity-40 z-[1]' ></div>
             <div className='con1  w-full h-[100px] absolute top-[8%]'>
                 <div className='con2'>
                     <h1 className='text-featured'>FEATURED VEHICLES</h1>
@@ -99,37 +120,155 @@ export const Homepage = () => {
                 </div>
                 
             </div>
-            <div className='slider absolute top-[30%] w-full pr-[10%] pl-[4%]'>
-            {cards.slice(currentIndex * cardsPerSlide, (currentIndex + 1) * cardsPerSlide).map((card) => (
+            <div className='slider absolute top-[22%] w-full pr-[10%] pl-[4%]'>
+            {cards.slice(currentIndex * cardsPerSlide, (currentIndex + 1) * cardsPerSlide).map(card => (
                     <div key={card.id} className="card">
                         <div className='relative'>
-                        <img className='img-slider' src={homepage} alt="" />
+                        <img className='img-slider' src={car1} alt="" />
                         <div className='absolute container-mini'>
                                 <h1 className='text-card'>${card.title}</h1>
-                                ndjvbm
+                                
                         </div>
                         </div>
-                        <div className='absolute top-[60%] bg-green-500 w-full h-[150px]'>
-
+                        <div className='absolute top-[63%]  w-full h-[140px] px-4'>
+                            <h1 className='title-section3'>{card.title}</h1>
+                            <h1 className='title1-section3'>{card.description}</h1>
+                        </div>
+                        <div className='absolute bottom-0 w-full  grid grid-cols-3 gap-1'>
+                            <div className='w-full h-[50px] bg-[#F1F1F1]'>
+                                <div className='flex justify-center h-full items-center mt-1'>
+                                    <div className='flex justify-center gap-1'>
+                                    <i class='bx bx-registered text-[1rem]' style={{color:'#ff5500'}}  ></i>
+                                    <h1 className='title3-section3 '>{card.title} mi</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='w-full h-[50px] bg-[#F1F1F1]'>
+                                <div className='flex justify-center h-full items-center mt-1'>
+                                    <div className='flex justify-center gap-1'>
+                                    <i class='bx bx-cog text-[1rem]' style={{color:'#ff5500'}}  ></i>
+                                    <h1 className='title3-section3 '>{card.title} mi</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='w-full h-[50px] bg-[#F1F1F1]'>
+                                <div className='flex justify-center h-full items-center mt-1'>
+                                    <div className='flex justify-center gap-1'>
+                                    <i class='bx bx-car text-[1rem]' style={{color:'#ff5500'}}  ></i>
+                                    <h1 className='title3-section3 '>{card.title} mi</h1>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                            
                         </div>
                     </div>
                 ))}
             </div>
-        </div>
-        {/* <div className="slider">
-                {cards.slice(currentIndex * cardsPerSlide, (currentIndex + 1) * cardsPerSlide).map((card) => (
-                    <div key={card.id} className="card">
-                        <h3>{card.title}</h3>
-                        <p>{card.description}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="dots">
+            <div className="dots absolute bottom-[20%] left-[50%]">
                 {[0, 1, 2].map((dotIndex) => (
                     <span key={dotIndex} onClick={() => handleDotClick(dotIndex)}
                           className={currentIndex === dotIndex ? 'dot active' : 'dot'}></span>
                 ))}
-            </div> */}
+            </div>
+            <div className='absolute bottom-0 w-full h-[150px]  px-[5%] z-[2]'>
+                    <form className='w-full h-full  pr-[12%] grid grid-cols-4 gap-3 items-center justify-center'>
+                    <Select  styles={customStyles} placeholder='All Branch' menuPosition='fixed' className='' options={options} />
+                    <Select placeholder='All Make' menuPosition='fixed' className='' options={options} />
+                    <Select placeholder='All Model' menuPosition='fixed' className='' options={options} />
+                    <Select placeholder='All Registration Date' menuPosition='fixed' className='' options={options} />
+                    
+                    </form>
+                    <button className='buttonSearch absolute top-[37%] right-[76px] px-[38px] py-[10px] text-white  bg-[#FF5500]'>Search</button>
+            </div>
+            
+            <div className='absolute h-[20vh] top-[86.2%] overlay inset-0  opacity-30 z-[1]' style={{backgroundImage: 'linear-gradient(to top, #FF5400 70%, #222222 100%)'}}></div>
+
+        </div>
+        <div className='section3.5 w-full h-[50vh] bg-white flex gap-3'>
+                    <div className='bg-pink-400 w-[50%] h-full px-10'>
+                        <div className='flex mt-[70px] gap-3'>
+                            <p className='text-browse'>Browse by make</p>
+                            <p className='text-show'>/ Show all make</p>
+                        </div>
+                        <div className='vertical-x-parallel mt-2'>
+                        <div className='line-x'></div>
+                        <div className='line-x mt-1'></div>                   
+                    </div>
+                    <div className='w-full h-full grid grid-cols-3 gap-2 mt-7'>
+                        <div className='w-full h-full bg-yellow-300'>
+                            <div className='flex gap-2'>
+                                <div className='bg-purple-400 w-[100px]'>
+                                    <img src={homepage2} alt="" />
+                                </div>
+                                <p>dvbfj</p>
+                            </div>
+                            <div className='flex'>
+                                <div className='bg-purple-400 w-[100px]'>
+                                    <img src={car1} alt="" />
+                                </div>
+                                <p>dvbfj</p>
+                            </div>
+                            <div className='flex'>
+                                <div className='bg-purple-400 w-[100px]'>
+                                    <img src={homepage} alt="" />
+                                </div>
+                                <p>dvbfj</p>
+                            </div>
+                        </div>
+                        <div className='w-full h-full bg-yellow-300'>
+
+                        </div>
+                        <div className='w-full h-full bg-yellow-300'>
+
+                        </div>
+                    </div>
+                    </div>
+                    <div className='bg-pink-400 w-[50%] h-full'>
+
+                    </div>
+        </div>
+        <div className='section4 w-full relative h-[50vh] bg-pink-500'>
+            <img className='w-full h-[50vh] object-cover object-center' src={homepage} alt="" />
+            <div className='absolute h-[50vh] overlay inset-0 bg-black  opacity-50 z-[1]'></div>
+            <div className='counter-container w-full h-full px-5  gap-2 absolute grid grid-cols-4 top-0'>
+                    <div className=' flex justify-center items-center relative'>
+                    <i class='bx bx-user-voice text-[8rem] absolute top-[22%] opacity-40' style={{color:'#fff'}}  ></i>
+                    <div className='absolute bottom-[29%] z-[3]'>
+                    <Counter  className='numbercount text-center' target={829} />
+                    <h1 className='titleCount text-center'>Happy clients</h1>
+                    </div>
+                    </div>
+                    <div className=' flex justify-center items-center relative'>
+                    <i class='bx bx-car text-[8rem] absolute top-[22%] opacity-40' style={{color:'#fff'}}  ></i>
+                    <div className='absolute bottom-[29%] z-[3]'>
+                    <Counter  className='numbercount text-center' target={1024} />
+                    <h1 className='titleCount text-center'>Vehicles in stock</h1>
+                    </div>
+                    </div>
+                    <div className=' flex justify-center items-center relative'>
+                    <i class='bx bx-award text-[8rem] absolute top-[22%] opacity-40' style={{color:'#fff'}}  ></i>
+                    <div className='absolute bottom-[29%] z-[3]'>
+                    <Counter  className='numbercount text-center' target={912} />
+                    <h1 className='titleCount text-center'>awards</h1>
+                    </div>
+                    </div>
+                    <div className=' flex justify-center items-center relative'>
+                    <i class='bx bx-group text-[8rem] absolute top-[22%] opacity-40' style={{color:'#fff'}}  ></i>
+                    <div className='absolute bottom-[29%] z-[3]'>
+                    <Counter  className='numbercount text-center' target={829} />
+                    <h1 className='titleCount text-center'>dealer branches</h1>
+                    </div>
+                    </div>
+                    
+            </div>
+
+        {/* <Counter target={829} /> */}
+        </div>
+        <div className='section4 w-full h-full bg-slate-400'>
+            <Select options={options} />
+        </div>
+        
     </>
   )
 }
---
+++ client/src/Components/SubFeature/Counter.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import '../ClientUI/Homepage/Homepage.css';
+
+const Counter = ({ target }) => {
+    const [count, setCount] = useState(0);
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.5  // Trigger khi 50% của phần tử hiện trên màn hình
+    });
+
+    useEffect(() => {
+        let interval = null;
+        if (inView && count < target) {
+            interval = setInterval(() => {
+                setCount((prevCount) => (prevCount + 1 <= target ? prevCount + 1 : target));
+            }, 0.5);  // Điều chỉnh tốc độ đếm
+        } else if (count === target && interval) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [inView, count, target]);
+
+    return (
+        <div  ref={ref}>
+            <h1 className='numbercount text-center'>{count}</h1>
+            
+        </div>
+    );
+};
+
+export default Counter;
