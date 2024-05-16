f3cc4759aa32e9fc0ece825bcf5de973cee5c7d5
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2024-05-16 07:48:12 +0700

    Update: Employee/Layout

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2321f079fd6090d7d2ea5fe86e1f85afaa1113e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb32b6a1a5131d7a899572c68e6a5e8391e97bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
