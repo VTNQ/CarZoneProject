fee495dc4cab3d13771cb05aa55b1fda578a6256
Kyos22 <nguyenthanhcongvt123@gmail.com>
2024-05-18 08:28:52 +0700

    update

---
+++ server/Controllers/CountriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    public class CountriesController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ server/Services/CountriesService.cs
@@ -0,0 +1,9 @@
+﻿using server.Data;
+
+namespace server.Services
+{
+    public interface CountriesService
+    {
+        public bool createCountries(AddCountries addCountries);
+    }
+}
