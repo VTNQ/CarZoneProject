e66ff05a71e41f842ced00a9f4ca81c2bcc2cd3e
Vo Tan Nam Quoc <115608795+VTNQ@users.noreply.github.com>
2024-05-25 09:40:49 +0700

    Update DatabaseContext.cs

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afdadc4a0a842e924bdaa427dcf94f6cda290ca6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a11d9604e4f66f235da5b6107ffb0090446839c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
